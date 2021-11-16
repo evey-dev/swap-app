df7f4fcd1e41090a2a7935260906b39bdc614f1c
eli <buip@bxscience.edu>
2021-11-16 01:01:29 -0500

    ForgotPasswordActivity and activity_forgot_password.xml

---
+++ app/src/main/java/com/example/swapapp/ForgotPasswordActivity.java
@@ -0,0 +1,43 @@
+package com.example.swapapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class ForgotPasswordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+        TextInputLayout email_input = findViewById(R.id.email_input);
+        Button button_back = findViewById(R.id.button_back);
+        Button button_confirm = findViewById(R.id.button_confirm);
+
+//        button_back.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+//            }
+//        });
+
+        button_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = email_input.getEditText().getText().toString();
+                if(email.equals(""))
+                    email_input.setError("Enter your email");
+                else if(!email.matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$"))
+                    email_input.setError("This is an invalid email, please enter a valid email");
+                else
+                    email_input.setErrorEnabled(false);
+                //add ooga booga codes or something
+            }
+        });
+    }
+}
