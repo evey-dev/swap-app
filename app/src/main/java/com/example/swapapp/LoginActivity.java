c7e410f811599d7040b26fa0fb67b04cdbdb45c6
Evey <evelyn.paskhaver@gmail.com>
2021-11-21 01:17:23 -0500

    Added firebase and updated Signin/Login to match

--- app/src/main/java/com/example/swapapp/LoginActivity.java
+++ app/src/main/java/com/example/swapapp/LoginActivity.java
@@ -4,41 +4,76 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
-
-import org.w3c.dom.Text;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class LoginActivity extends Activity {
+    private FirebaseAuth auth;
+    TextInputLayout emailInput;
+    TextInputLayout passwordInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextInputLayout usernameInput = findViewById(R.id.username_input);
-        TextInputLayout passwordInput = findViewById(R.id.password_input);
+
+        auth = FirebaseAuth.getInstance();
+        emailInput = findViewById(R.id.email_input);
+        passwordInput = findViewById(R.id.password_input);
+
         findViewById(R.id.button_confirm).setOnClickListener(view -> {
+            String email = emailInput.getEditText().getText().toString();
+            String password = passwordInput.getEditText().getText().toString();
             boolean error = false;
-            if (TextUtils.isEmpty(usernameInput.getEditText().getText().toString())) {
-                usernameInput.setError("Please enter your username");
+            if (TextUtils.isEmpty(email)) {
+                emailInput.setError("Please enter your username");
                 error = true;
             }
-            else usernameInput.setErrorEnabled(false);
-            if (TextUtils.isEmpty(passwordInput.getEditText().getText().toString())) {
+            else emailInput.setErrorEnabled(false);
+            if (TextUtils.isEmpty(password)) {
                 passwordInput.setError("Please enter your password");
                 error = true;
             }
             else passwordInput.setErrorEnabled(false);
             if (error) return;
+            loginUser(email, password);
         });
+
         findViewById(R.id.button_forgot_password).setOnClickListener(view -> {
             startActivity(new Intent(LoginActivity.this, ForgotPasswordActivity.class));
             finish();
         });
-//        findViewById(R.id.button_sign_up).setOnClickListener(view -> {
-//            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
-//        });
+        findViewById(R.id.button_sign_up).setOnClickListener(view -> {
+            startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+            finish();
+        });
+    }
+
+    private void loginUser(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> {
+                })
+                .addOnCompleteListener(task -> {
+                    if(!task.isSuccessful()) {
+                        try {
+                            throw task.getException();
+                        } catch(FirebaseAuthInvalidUserException e) {
+                            emailInput.setError("Email or password does not match");
+                        } catch(FirebaseAuthInvalidCredentialsException e) {
+                            passwordInput.setError("Email or password does not match");
+                        } catch(Exception e) {
+                            Log.e("login", e.getMessage());
+                        }
+                    }
+                    else
+                        Toast.makeText(LoginActivity.this, "log in completed", Toast.LENGTH_SHORT).show();
+                });
     }
 }
--- app/src/main/java/com/example/swapapp/SignupActivity.java
+++ app/src/main/java/com/example/swapapp/SignupActivity.java
@@ -4,52 +4,86 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.ktx.Firebase;
 
 public class SignupActivity extends Activity {
+    private FirebaseAuth auth;
+    TextInputLayout passwordInput;
+    TextInputLayout emailInput;
+    TextInputLayout repeatInput;
+    boolean result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        TextInputLayout emailInput = findViewById(R.id.email_input);
-        TextInputLayout repeatInput = findViewById(R.id.input_repeat_password_hint);
-        TextInputLayout usernameInput = findViewById(R.id.username_input);
-        TextInputLayout passwordInput = findViewById(R.id.password_input);
+
+        auth = FirebaseAuth.getInstance();
+        result = false;
+        emailInput = findViewById(R.id.email_input);
+        repeatInput = findViewById(R.id.input_repeat_password_hint);
+        passwordInput = findViewById(R.id.password_input);
+
         findViewById(R.id.button_confirm).setOnClickListener(view -> {
             boolean error = false;
-            if (TextUtils.isEmpty(usernameInput.getEditText().getText().toString())) {
-                usernameInput.setError("Please enter your username");
+            String email = emailInput.getEditText().getText().toString();
+            String password = passwordInput.getEditText().getText().toString();
+            String repeat_password = repeatInput.getEditText().getText().toString();
+            if (TextUtils.isEmpty(email)) {
+                emailInput.setError("Please enter your email");
                 error = true;
             }
-            else usernameInput.setErrorEnabled(false);
-            if (TextUtils.isEmpty(passwordInput.getEditText().getText().toString())) {
+            else emailInput.setErrorEnabled(false);
+            if (TextUtils.isEmpty(password)) {
                 passwordInput.setError("Please enter your password");
                 error = true;
             }
             else passwordInput.setErrorEnabled(false);
-            if (TextUtils.isEmpty(emailInput.getEditText().getText().toString())) {
-                emailInput.setError("Please enter your email");
-                error = true;
-            }
-            else if(!emailInput.getEditText().getText().toString().matches("^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$"))
-                emailInput.setError("This is an invalid email, please enter a valid email");
-            else emailInput.setErrorEnabled(false);
-            if (TextUtils.isEmpty(repeatInput.getEditText().getText().toString())) {
+            if (TextUtils.isEmpty(repeat_password)) {
                 repeatInput.setError("Please repeat your password");
                 error = true;
             }
+            else if (!repeat_password.equals(password)) {
+                repeatInput.setError("Passwords must match");
+            }
             else repeatInput.setErrorEnabled(false);
             if (error) return;
-
+            registerUser(email, password);
+            if (!result) return;
         });
-    //    findViewById(R.id.button_log_in).setOnClickListener(view -> {
-    //        LoginActivity(new Intent(LoginActivity.this));
-    //        finish();
-    //    });
 
-
-        };
+        findViewById(R.id.button_log_in).setOnClickListener(view -> {
+            startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+            finish();
+        });
     }
+
+    private void registerUser(String email, String password) {
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(SignupActivity.this, task -> {
+            if (task.isSuccessful())
+                result = true;
+            if(!task.isSuccessful()) {
+                try {
+                    throw task.getException();
+                } catch(FirebaseAuthWeakPasswordException e) {
+                    passwordInput.setError(((FirebaseAuthWeakPasswordException) task.getException()).getReason());
+                } catch(FirebaseAuthInvalidCredentialsException e) {
+                    emailInput.setError("Enter a valid email");
+                } catch(FirebaseAuthUserCollisionException e) {
+                    emailInput.setError("An account under this email already exists");
+                } catch(Exception e) {
+                    Log.e("signup", e.getMessage());
+                }
+            }
+        });
+    }
+}
 
