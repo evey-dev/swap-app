000aa610cbf3de422132275628539a7190a330e3
Evey <evelyn.paskhaver@gmail.com>
2021-11-23 02:17:30 -0500

    homeactivity listview (mostly) works

--- app/src/main/java/com/example/swapapp/HomeActivity.java
+++ app/src/main/java/com/example/swapapp/HomeActivity.java
@@ -22,23 +22,6 @@ public class HomeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        int currentApiVersion = android.os.Build.VERSION.SDK_INT;
-
-        final int flags = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-
-        if(currentApiVersion >= Build.VERSION_CODES.KITKAT) {
-            getWindow().getDecorView().setSystemUiVisibility(flags);
-            final View decorView = getWindow().getDecorView();
-            decorView.setOnSystemUiVisibilityChangeListener(visibility -> {
-                if((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0)
-                    decorView.setSystemUiVisibility(flags);
-            });
-        }
 
         user = FirebaseAuth.getInstance().getCurrentUser();
         FirebaseDatabase db = FirebaseDatabase.getInstance();
@@ -49,6 +32,10 @@ public class HomeActivity extends Activity {
 
         ListView itemList = findViewById(R.id.item_list);
         itemList.setEmptyView(findViewById(R.id.empty_list_text));
+
+        String[] ids = {"abcdefg", "abcdefg1","abcdefg1","abcdefg1","abcdefg1","abcdefg1","abcdefg1"};
+        ItemListAdapter adapter = new ItemListAdapter(this, ids);
+        itemList.setAdapter(adapter);
 
 
     }
---
+++ app/src/main/java/com/example/swapapp/ItemListAdapter.java
@@ -0,0 +1,76 @@
+package com.example.swapapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ItemListAdapter extends BaseAdapter {
+    Context context;
+    String[] ids;
+    private static LayoutInflater inflater = null;
+
+    public ItemListAdapter(Context context, String[] ids) {
+        this.context = context;
+        this.ids = ids;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return ids.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return ids[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View v = convertView;
+        if (v == null)
+            v = inflater.inflate(R.layout.list_item, null);
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("items").child(ids[position]);
+        TextView text = v.findViewById(R.id.item_name);
+        dbRef.child("name").get().addOnCompleteListener(task ->
+                text.setText(String.valueOf(task.getResult().getValue())));
+
+        CircleImageView image = v.findViewById(R.id.item_image);
+
+        dbRef.child("image").get().addOnCompleteListener(task -> {
+            StorageReference photoReference = FirebaseStorage.getInstance().getReference().child(String.valueOf(task.getResult().getValue()));
+            try {
+                final File localFile = File.createTempFile("temp","jpg");
+                photoReference.getFile(localFile)
+                        .addOnSuccessListener(taskSnapshot -> {
+                            Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            image.setImageBitmap(bitmap);
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return v;
+    }
+}
